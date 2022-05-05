62bf610c103bec883bf931a81c43cd40c1422931
NeyraLucasLE <luis-enrique.neyra@atos.net>
2022-05-05 12:19:14 -0500

    Add branch

--- CleanArquitectureALTA/Startup.cs
+++ CleanArquitectureALTA/Startup.cs
@@ -19,6 +19,7 @@ namespace CleanArquitectureALTA
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
