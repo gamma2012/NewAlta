c0acc7f542dcec5e2f36507746887a5596512900
kurayamigin <edwin-julian.gonzalez@atos.net>
2022-05-09 10:24:58 -0500

    sonar scripts

---
+++ devops/sonar/init-sonar.ps1
@@ -0,0 +1,8 @@
+﻿docker-compose up -d db
+wsl -d docker-desktop sysctl -w vm.max_map_count=262144
+dotnet tool install --global dotnet-sonarscanner
+dotnet tool install --global dotnet-coverage
+docker exec sonar-db-container psql -d postgres -c "CREATE DATABASE sonar"
+
+docker-compose up sonarqube
+
