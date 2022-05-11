cc8bb7161ef720d852b1e622d90c7b8e1029ea48
Alfonso Carmona <alfonso.carmona.94@outlook.com>
2022-05-11 11:53:50 -0500

    Done login endpoint definition, userDTO, Intput Output port

---
+++ src/ApplicationBusinessRules/CleanArquitectureALTA.UseCases/Interactors/LoginInteractor.cs
@@ -0,0 +1,24 @@
+﻿using Alta.DTOs;
+using Alta.UseCasesPorts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alta.UseCases.Interactors
+{
+    public class LoginInteractor : ILoginInputPort
+    {
+        private readonly ILoginOutputPort _loginOutputPort;
+        public LoginInteractor(ILoginOutputPort loginOutputPort)
+        {
+            _loginOutputPort = loginOutputPort;
+        }
+        public Task Handle(UserDTO user)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/InterfaceAdapters/Controllers/CleanArquitectureALTA.Controllers/Login/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Alta.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Alta.Controllers.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody]UserDTO user)
+        {
+            return null;
+        }
+
+
+
+        //Revisar si es necesario
+        [HttpGet("/username/{username}/password/{password}")]
+
+        public async Task<IActionResult> LoginByUsernameAndPassword(string username, string password)
+        {
+            return null;
+        }
+
+        [Authorize]
+        [HttpGet("/CheckLogin")]
+
+        public async Task<IActionResult> CheckLogin()
+        {
+            return null;
+        }
+    }
+}
